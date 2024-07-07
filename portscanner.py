cc04a9d70b16801433c575b706fc32280059e4e1
Pharos <168114080+Pharosw@users.noreply.github.com>
2024-07-07 14:15:44 +0300

    portscanner.py

---
+++ portscanner.py
@@ -0,0 +1,119 @@
+
+from datetime import datetime,timedelta
+from colorama import Fore,Style,init
+import socket
+import os
+import sys
+import time
+init(autoreset=True)
+print(Fore.RED + '''
+
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡠⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠙⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠋⠀⠀⠀⠀⠀⠀⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠾⢛⠒⠀⠀⠀⠀⠀⠀⠀⢸⡆⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣶⣄⡈⠓⢄⠠⡀⠀⠀⠀⣄⣷⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣷⠀⠈⠱⡄⠑⣌⠆⠀⠀⡜⢻⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡿⠳⡆⠐⢿⣆⠈⢿⠀⠀⡇⠘⡆⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣷⡇⠀⠀⠈⢆⠈⠆⢸⠀⠀⢣⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣧⠀⠀⠈⢂⠀⡇⠀⠀⢨⠓⣄⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣦⣤⠖⡏⡸⠀⣀⡴⠋⠀⠈⠢⡀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⠁⣹⣿⣿⣿⣷⣾⠽⠖⠊⢹⣀⠄⠀⠀⠀⠈⢣⡀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⣇⣰⢫⢻⢉⠉⠀⣿⡆⠀⠀⡸⡏⠀⠀⠀⠀⠀⠀⢇
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢨⡇⡇⠈⢸⢸⢸⠀⠀⡇⡇⠀⠀⠁⠻⡄⡠⠂⠀⠀⠀⠘
+  ⢤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠛⠓⡇⠀⠸⡆⢸⠀⢠⣿⠀⠀⠀⠀⣰⣿⣵⡆⠀⠀⠀⠀
+  ⠈⢻⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡿⣦⣀⡇⠀⢧⡇⠀⠀⢺⡟⠀⠀⠀⢰⠉⣰⠟⠊⣠⠂⠀⡸
+  ⠀⠀⢻⣿⣿⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⢧⡙⠺⠿⡇⠀⠘⠇⠀⠀⢸⣧⠀⠀⢠⠃⣾⣌⠉⠩⠭⠍⣉⡇
+  ⠀⠀⠀⠻⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣞⣋⠀⠈⠀⡳⣧⠀⠀⠀⠀⠀⢸⡏⠀⠀⡞⢰⠉⠉⠉⠉⠉⠓⢻⠃
+  ⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⢀⣀⠠⠤⣤⣤⠤⠞⠓⢠⠈⡆⠀⢣⣸⣾⠆⠀⠀⠀⠀⠀⢀⣀⡼⠁⡿⠈⣉⣉⣒⡒⠢⡼⠀
+  ⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣎⣽⣶⣤⡶⢋⣤⠃⣠⡦⢀⡼⢦⣾⡤⠚⣟⣁⣀⣀⣀⣀⠀⣀⣈⣀⣠⣾⣅⠀⠑⠂⠤⠌⣩⡇⠀
+  ⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡁⣺⢁⣞⣉⡴⠟⡀⠀⠀⠀⠁⠸⡅⠀⠈⢷⠈⠏⠙⠀⢹⡛⠀⢉⠀⠀⠀⣀⣀⣼⡇⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⡟⢡⠖⣡⡴⠂⣀⣀⣀⣰⣁⣀⣀⣸⠀⠀⠀⠀⠈⠁⠀⠀⠈⠀⣠⠜⠋⣠⠁⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⡟⢿⣿⣿⣷⡟⢋⣥⣖⣉⠀⠈⢁⡀⠤⠚⠿⣷⡦⢀⣠⣀⠢⣄⣀⡠⠔⠋⠁⠀⣼⠃⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⡄⠈⠻⣿⣿⢿⣛⣩⠤⠒⠉⠁⠀⠀⠀⠀⠀⠉⠒⢤⡀⠉⠁⠀⠀⠀⠀⠀⢀⡿⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣤⣤⠴⠟⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠤⠀⠀⠀⠀⠀⢩⠇⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ PHAROS⠀
+  ''')
+portlar = [20,21,23,25,53,67,68,80,88,110,137,138,139,140,161,443,445,465,514,520,587,1443,1723,3723,3006,3389,5900,8008,8080,8443,993,162,1434,1521,3389,5632,5432]
+calisiyor = True
+while calisiyor:
+    print(Fore.BLUE +''' Pharos Port Scannera Hoşgeldiniz Aşagıda yapabileceğiniz işlemler mevcuttur:
+    1. Otomatik Port Taraması yapar
+    2. Port aralıklarını siz belirlersiniz
+    3. Yapımcılar -Z
+    4. yardım -h
+    5. çıkış
+    6. hedef sitenin ip'sini size yansitir
+    ''')
+    secim = input(Fore.GREEN +">>>> ")
+    print(Style.RESET_ALL)
+    if secim == '1':
+      ip = input("Bir ip gir: ")
+      print('--' * 40)
+      print('Başlanğıç tarihi: ' + str(datetime.now()))
+      print('hedef ip: {0}'.format(ip))
+      print('--' * 40)
+      for port in portlar:
+        try:
+          tara = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+          tara.settimeout(0.3)
+          result = tara.connect_ex((ip,port))
+
+          if result == 0:
+            print(Fore.GREEN + f"{port}  portu Açık [*]")
+
+          else:
+            print(Fore.RED + f"{port} portu kapalı ")
+
+        except socket.error:
+          print("bu siteye bağlanılamadı yada yanlış girmiş olabilirsin")
+
+    elif secim == '2':
+      en_dusuk_port = int(input("En dusuk portu gir: "))
+      en_yuksek_port = int(input("En yuksek portu gir: "))
+      ip = input("Bir ip gir: ")
+      print('--' * 40)
+      print('Başlangıç tarihi: ' + str(datetime.now()))
+      print(f'Hedef ip: {ip}')
+      print('--' * 40)
+      for port in range(en_dusuk_port, en_yuksek_port+1):
+        try:
+          tara = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+          tara.settimeout(0.1)
+          result = tara.connect_ex((ip,port))
+
+          if result == 0:
+            print(Fore.GREEN + f'{port} portu açık [*]')
+
+          else:
+            print(Fore.RED + f'{port} portu kapalı ')
+        except socket.error:
+          print('Bu siteye bağlanılamadı yada yanlış girmiş olabilirsin.')
+
+    elif '3' in secim:
+      print('''Bu Arac Pharos Tarafından yapılmıştır Şuanlık Geliştirilme Aşamasındadır En Kısa Sürede Bu Aracın Daha İyi Bir Şekliyle Karşınızda Olacağım.Beni tercih ettiginiz içinde teşekkürler.\n''')
+
+    elif '4' in secim:
+      print('''Merhabalar dostum,Buraya geldiğine göre herhalde yardıma ihtiyacın var
+      1. seçenek otomatik tarama yapar yani belirlediğim popüler portları tarar
+      2. seçenek port aralığını siz belirlersiniz baslangic 100 bitis 200 gibi
+      3. seçenek bu toolun geliştiricisi hakkında bilgi verir
+      4. seçenek buradasın işte yardımcı olur😀
+      5. seçenek çıkış yapar
+      6. seçenek hedef sitenin ip'sini gösterir
+      ''')
+    elif '5' in secim:
+      print("Görüşmek üzere")
+      time.sleep(0.7)
+      os.system('clear')
+      exit()
+    elif secim == '6':
+      try:
+         hedef = input("Bir Hedef site giriniz: ")
+         al = socket.gethostbyname(hedef)
+         print(Fore.YELLOW + f'{al} şanslı günündesin 😀')
+
+      except socket.gaierror:
+         print(f"{hedef} adında girdiğiniz web sitesi bulunamadı yada yanlış girmiş olabilir misin?")
+    else:
+      print(f"Girdiğiniz {secim} bulunamadı yanlış girmiş olabilir misin?")
